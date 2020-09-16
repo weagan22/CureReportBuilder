d5889247b50209b72bc6a595630440ceaa630de0
Will Eagan <Will.Eagan@WILLE-PC>
2020-09-16 11:05:13 -0700

    Rolled rev to 0.3.3

--- CureReportBuilder/MainForm.Designer.vb
+++ CureReportBuilder/MainForm.Designer.vb
@@ -1492,7 +1492,7 @@ Partial Class MainForm
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.MainMenuStrip = Me.MenuStrip1
         Me.Name = "MainForm"
-        Me.Text = "Cure Report Builder 0.3.2"
+        Me.Text = "Cure Report Builder 0.3.3"
         Me.MenuStrip1.ResumeLayout(False)
         Me.MenuStrip1.PerformLayout()
         Me.StatusStrip1.ResumeLayout(False)
